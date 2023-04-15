c7c99ca4072b3b0f871afc07ef4512b566869f11
Shulaev Ivan <mail@ntdservice.ru>
2023-04-15 21:20:37 +0300

    ready

--- src/components/DropdownItem/DropdownItem.jsx
+++ src/components/DropdownItem/DropdownItem.jsx
@@ -3,25 +3,8 @@ import { useState } from "react";
 
 const DropdownItem = (props) => {
     const [itemState, setItemState] = useState(props.item);
-    
-    const handleClick = (e) => {
-        e.preventDefault();
-        if (itemState.status === '') {
-            setItemState({
-                ...itemState,
-                status: 'active'
-            })
-        }
-        else if (itemState.status === 'active') {
-            setItemState({
-                ...itemState,
-                status: ''
-            })
-        }
-        
-    }
     return (
-        <li className={itemState.status}><a href="#"  onClick={handleClick}>{itemState.content}</a></li>
+        <li className={itemState.status} data-id={itemState.id}><a href="#"  onClick={itemState.event}>{itemState.content}</a></li>
     )
 }
 
--- src/components/DropdownList/DropdownList.jsx
+++ src/components/DropdownList/DropdownList.jsx
@@ -6,33 +6,53 @@ const DropdownList = (props) => {
     const inlineStyle = {
         visibility: props.status,
     };
-    const listItems = [
+    const swapStatus = (e) => {
+        const tmpStatus = [];
+        e.preventDefault();
+        const id = Number(e.target.parentNode.getAttribute('data-id'));
+        listItems.map((item, i) => {
+            item.status = '';
+            if (item.id === id)  {
+                item.status = 'active';
+            }
+            tmpStatus.push(item);
+            setListItems(tmpStatus)
+        })
+    }
+    const initialState = [
         {
             id: 1,
             status: 'active',
             content: 'Profile Information',
-        },
-        {
-            status: '',
-            content: 'Change Password'
+            event: swapStatus,
         },
         {
             id: 2,
             status: '',
-            content: 'Become PRO'
+            content: 'Change Password',
+            event: swapStatus,
         },
         {
             id: 3,
             status: '',
-            content: 'Help'
+            content: 'Become PRO',
+            event: swapStatus,
         },
         {
             id: 4,
             status: '',
-            content: 'Log Out'
+            content: 'Help',
+            event: swapStatus,
+        },
+        {
+            id: 5,
+            status: '',
+            content: 'Log Out',
+            event: swapStatus,
         },
     ]
-    
+    const [listItems, setListItems] = useState(initialState);
+
     return (
 
         <ul data-id="dropdown" className="dropdown" style={inlineStyle}>
